1841713ddd2e45173a6ed52d57a363dc18f52f73
Benk0033 <benk0033@gmail.com>
2019-04-30 20:22:45 -0700

    added markers based on store locator api

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,12 +11,11 @@ function initMap() {
     };
     // var map = new google.maps.Map($('#map'), options);
     //issues with using map api & jquery...
-    var map = new google.maps.Map(document.getElementById('map'), options);
-
-    var map, infoWindow;
+    map = new google.maps.Map(document.getElementById('map'), options);
 
     infoWindow = new google.maps.InfoWindow;
 
+    // detects user location
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
 
@@ -38,63 +37,81 @@ function initMap() {
         handleLocationError(false, infoWindow, map.getCenter());
     }
 
-    var queryURL = "https://storelocator.velvethammerbranding.com/api/v1/dmhfc3RvcmVsb2NhdG9yLXYxeyJjaWQiOjJ9/get-stores/34.0522342/-118.2436849/5";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-    });
-    // Make request to API and get response before this point.
-    var json = response.getContentText();
-    var data = JSON.parse(json);
-    console.log(data);
+    var zip = $('#location-input').val();
+    var latitude = 34.0522342;
+    var longitude = -118.2436849;
+    var distance = 25;
+    var queryURL = "https://storelocator.velvethammerbranding.com/api/v1/dmhfc3RvcmVsb2NhdG9yLXYxeyJjaWQiOjJ9/get-stores/" + latitude + "/" + longitude + "/" + distance;
 
-    var marker = [
-        {
-            coords: { lat: 34.0522, lng: -118.2437 },
-            iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            content: '<h3> Store Location Information Here</h3>'
-        },
-        {
-            coords: { lat: 34.0689, lng: -118.4452 },
-            // iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            content: '<h3> Store Location Information Here</h3>'
-        },
-        {
-            coords: { lat: 34.0195, lng: -118.4912 },
-            iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            content: '<h3> Store Location Information Here</h3>'
-        }
-    ];
+    function storeMarkers() {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
 
+            console.log(JSON.parse(response));
+            var JSONObject = JSON.parse(response);
 
-    for (var i = 0; i < marker.length; i++) {
-        addMarker(marker[i]);
-    };
-    //add marker function
-    function addMarker(props) {
-        var marker = new google.maps.Marker({
-            position: props.coords,
-            map: map,
-            // icon: props.iconImage
+            for (var i = 0; i < JSONObject.stores.length; i++) {
+
+                var storesLat = JSONObject.stores[i].lat;
+                var storesLng = JSONObject.stores[i].lng;
+
+                var marker = new google.maps.Marker({ position: { lat: parseFloat(storesLat), lng: parseFloat(storesLng) }, map: map });
+
+            };
+
         });
-        //checks for icon image
-        if (props.iconImage) {
-            //set icon image
-            marker.setIcon(props.iconImage);
-        }
-        // similar check for content...
-        if (props.content) {
-            var infoWindow = new google.maps.InfoWindow({
-                content: props.content
-            });
-
-            marker.addListener('click', function () {
-                infoWindow.open(map, marker);
-            });
-        };
-
     };
+
+    storeMarkers();
+
+    // var marker = [
+    //     {
+    //         coords: { lat: 34.0522, lng: -118.2437 },
+    //         iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    //         content: '<h3> Store Location Information Here</h3>'
+    //     },
+    //     {
+    //         coords: { lat: 34.0689, lng: -118.4452 },
+    //         // iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    //         content: '<h3> Store Location Information Here</h3>'
+    //     },
+    //     {
+    //         coords: { lat: 34.0195, lng: -118.4912 },
+    //         iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+    //         content: '<h3> Store Location Information Here</h3>'
+    //     }
+    // ];
+
+
+    // for (var i = 0; i < marker.length; i++) {
+    //     addMarker(marker[i]);
+    // };
+    // //add marker function
+    // function addMarker(props) {
+    //     var marker = new google.maps.Marker({
+    //         position: props.coords,
+    //         map: map,
+    //         // icon: props.iconImage
+    //     });
+    //     //checks for icon image
+    //     if (props.iconImage) {
+    //         //set icon image
+    //         marker.setIcon(props.iconImage);
+    //     }
+    //     // similar check for content...
+    //     if (props.content) {
+    //         var infoWindow = new google.maps.InfoWindow({
+    //             content: props.content
+    //         });
+
+    //         marker.addListener('click', function () {
+    //             infoWindow.open(map, marker);
+    //         });
+    //     };
+
+    // };
 };
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -105,125 +122,109 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.open(map);
 };
 
-// var marker = [
-//     {
-//         coords: { lat: 34.0522, lng: -118.2437 },
-//         iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-//         content: '<h3> Store Location Information Here</h3>'
-//     },
-//     {
-//         coords: { lat: 34.0689, lng: -118.4452 },
-//         // iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-//         content: '<h3> Store Location Information Here</h3>'
-//     },
-//     {
-//         coords: { lat: 34.0195, lng: -118.4912 },
-//         iconImage: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-//         content: '<h3> Store Location Information Here</h3>'
-//     }
-// ];
-// for (var i = 0; i < marker.length; i++) {
-//     addMarker(marker[i]);
-// };
-// //add marker function
-// function addMarker(props) {
-//     var marker = new google.maps.Marker({
-//         position: props.coords,
-//         map: map,
-//         // icon: props.iconImage
+
+// var zip = $('#location-input').val();
+// var latitude = 34.0522342;
+// var longitude = -118.2436849;
+// var distance = 5;
+// var queryURL = "https://storelocator.velvethammerbranding.com/api/v1/dmhfc3RvcmVsb2NhdG9yLXYxeyJjaWQiOjJ9/get-stores/" + latitude + "/" + longitude + "/" + distance;
+
+// function findLocation() {
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+
+//         console.log(JSON.parse(response));
+//         var JSONObject = JSON.parse(response);
+//         // var products = JSONObject.products;
+//         // var retailers = JSONObject.retailers;
+//         // var stores = JSONObject.stores;
+//         // console.log(products);
+
+//         // 1. generate html markup for retailers
+//         // 2. append html
+//         for (var i = 0; i < JSONObject.products.length; i++) {
+//             var productName = JSONObject.products[i].name;
+//             var productID = JSONObject.products[i].id;
+//             var productTitle = JSONObject.products[i].title;
+//             // console.log(productName);
+//             // console.log(productID);
+//             // console.log(productTitle);
+//         }
+
 //     });
-//     //checks for icon image
-//     if (props.iconImage) {
-//         //set icon image
-//         marker.setIcon(props.iconImage);
-//     }
-//     // similar check for content...
-//     if (props.content) {
-//         var infoWindow = new google.maps.InfoWindow({
-//             content: props.content
-//         });
-
-//         marker.addListener('click', function () {
-//             infoWindow.open(map, marker);
-//         });
-//     };
-
 // };
 
+// findLocation();
 
 
-var zip = $('#location-input').val()
-var latitude = 34.0522342;
-var longitude = -118.2436849;
-var distance = 25;
-var queryURL = "https://storelocator.velvethammerbranding.com/api/v1/dmhfc3RvcmVsb2NhdG9yLXYxeyJjaWQiOjJ9/get-stores/" + latitude + "/" + longitude + "/" + distance;
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     // var obj = JSON.parse(response);
+//     // console.log(obj)
+//     // console.log(obj.stores[0].lat);
+//     // console.log(obj.stores[0].lng);
+//     // // var storeLat = obj.stores[0].lat
+//     // // var storeLng = obj.stores[0].lng;
+//     // //try to parse out the JSON information to specific data
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    // var obj = JSON.parse(response);
-    // console.log(obj)
-    // console.log(obj.stores[0].lat);
-    // console.log(obj.stores[0].lng);
-    // // var storeLat = obj.stores[0].lat
-    // // var storeLng = obj.stores[0].lng;
-    // //try to parse out the JSON information to specific data
+//     // for (var i = 0; i < obj.stores.length; i++) {
+//     //     $("#JSON").html(obj.stores[i])
+//     // }
 
-    // for (var i = 0; i < obj.stores.length; i++) {
-    //     $("#JSON").html(obj.stores[i])
-    // }
+//     //    console.log(typeof response);
+//     console.log(JSON.parse(response));
+//     var JSONObject = JSON.parse(response);
+//     var products = JSONObject.products;
+//     var retailers = JSONObject.retailers;
+//     var stores = JSONObject.stores;
+//     console.log(products);
 
-    //    console.log(typeof response);
-    console.log(JSON.parse(response));
-    var JSONObject = JSON.parse(response);
-    var products = JSONObject.products;
-    var retailers = JSONObject.retailers;
-    var stores = JSONObject.stores;
-    console.log(products);
+//     // 1. generate html markup for retailers
+//     // 2. append html
+//     for (var i = 0; i < JSONObject.products.length; i++) {
+//         var productName = JSONObject.products[i].name;
+//         var productID = JSONObject.products[i].id;
+//         var productTitle = JSONObject.products[i].title;
+//         console.log(productName);
+//         console.log(productID);
+//         console.log(productTitle);
+//     }
 
-    // 1. generate html markup for retailers
-    // 2. append html
-    for (var i = 0; i < JSONObject.products.length; i++) {
-        var productName = JSONObject.products[i].name;
-        var productID = JSONObject.products[i].id;
-        var productTitle = JSONObject.products[i].title;
-        console.log(productName);
-        console.log(productID);
-        console.log(productTitle);
-    }
+//     for (var i = 0; i < JSONObject.retailers.length; i++) {
+//         var retailerName = JSONObject.retailers[i].name;
+//         var retailerID = JSONObject.retailers[i].id;
+//         console.log(retailerName);
+//         console.log(retailerID);
+//     }
 
-    for (var i = 0; i < JSONObject.retailers.length; i++) {
-        var retailerName = JSONObject.retailers[i].name;
-        var retailerID = JSONObject.retailers[i].id;
-        console.log(retailerName);
-        console.log(retailerID);
-    }
+//     for (var i = 0; i < JSONObject.stores.length; i++) {
+//         var storeAddress = JSONObject.stores[i].address;
+//         var storeCity = JSONObject.stores[i].city;
+//         var storeCountry = JSONObject.stores[i].country;
+//         var storeDistance = JSONObject.stores[i].distance;
+//         var storeID = JSONObject.stores[i].id;
+//         var storeLatitude = JSONObject.stores[i].lat;
+//         var storeLongitude = JSONObject.stores[i].lng;
+//         var storeProducts = JSONObject.stores[i].products;
+//         var storeRetailer = JSONObject.stores[i].retailer;
+//         var storeState = JSONObject.stores[i].state;
+//         var storeZip = JSONObject.stores[i].zip;
+//         console.log(storeAddress);
+//         console.log(storeCity);
+//         console.log(storeCountry);
+//         console.log(storeDistance);
+//         console.log(storeID);
+//         console.log(storeLatitude);
+//         console.log(storeLongitude);
+//         console.log(storeProducts);
+//         console.log(storeRetailer);
+//         console.log(storeState);
+//         console.log(storeZip);
+//     }
 
-    for (var i = 0; i < JSONObject.stores.length; i++) {
-        var storeAddress = JSONObject.stores[i].address;
-        var storeCity = JSONObject.stores[i].city;
-        var storeCountry = JSONObject.stores[i].country;
-        var storeDistance = JSONObject.stores[i].distance;
-        var storeID = JSONObject.stores[i].id;
-        var storeLatitude = JSONObject.stores[i].lat;
-        var storeLongitude = JSONObject.stores[i].lng;
-        var storeProducts = JSONObject.stores[i].products;
-        var storeRetailer = JSONObject.stores[i].retailer;
-        var storeState = JSONObject.stores[i].state;
-        var storeZip = JSONObject.stores[i].zip;
-        console.log(storeAddress);
-        console.log(storeCity);
-        console.log(storeCountry);
-        console.log(storeDistance);
-        console.log(storeID);
-        console.log(storeLatitude);
-        console.log(storeLongitude);
-        console.log(storeProducts);
-        console.log(storeRetailer);
-        console.log(storeState);
-        console.log(storeZip);
-    }
-
-});
+// });
 
