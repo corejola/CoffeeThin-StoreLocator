20ae6395a778a579980b573e8fcaf458a03ca920
Hinojos <nhinojos@deloitte.com>
2019-05-04 00:24:13 -0700

    adding code to differntiate icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 var map;
 var markers = [];
+//this array stores a different icon based on user location input
+var markerLocation = [];
 var infoWindow;
 
 // initializes the map when page loads
@@ -8,7 +10,7 @@ function initMap() {
         zoom: 10,
         center: { lat: 34.0522, lng: -118.2437 }
     };
-  
+
     // stores new map object into a variable
 
     map = new google.maps.Map(document.getElementById('map'), options);
@@ -32,13 +34,14 @@ function initMap() {
             map.setCenter(pos);
 
             // places marker at user location when their location is detected
-            var marker = new google.maps.Marker({
+            var markerLocation = new google.maps.Marker({
                 position: pos,
                 map: map,
                 icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
             });
 
-            markers.push(marker);
+            //pushing markers to the user location array, not the store location array
+            markerLocation.push(markerLocation);
 
 
             // handles errors when user does not agree to let browser detect their location
@@ -92,19 +95,20 @@ function initMap() {
                 resultsMap.setCenter(results[0].geometry.location);
 
                 // places a marker at user input location
-                var marker = new google.maps.Marker({
+                var markerLocation = new google.maps.Marker({
                     map: resultsMap,
                     position: results[0].geometry.location,
                     icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
                 });
-
-                markers.push(marker);
+                //clears out location markers for new user input
+                removeLocationMarkers();
+                markerLocation.push(markerLocation);
 
                 // commented this out because it placed a marker at search location
-                var marker = new google.maps.Marker({
-                    map: resultsMap,
-                    position: results[0].geometry.location
-                });
+                // var marker = new google.maps.Marker({
+                //     map: resultsMap,
+                //     position: results[0].geometry.location
+                // });
 
 
             } else {
@@ -117,7 +121,7 @@ function initMap() {
             var key = "dmhfc3RvcmVsb2NhdG9yLXYxeyJjaWQiOjJ9";
             var latitude = results[0].geometry.location.lat();
             var longitude = results[0].geometry.location.lng();
-            var distance = document.getElementById('miles-input').value;;
+            var distance = document.getElementById('miles-input').value;
             var queryURL = "https://storelocator.velvethammerbranding.com/api/v1/" + key + "/get-stores/" + latitude + "/" + longitude + "/" + distance;
 
             // display markers for nearby store locations relative to searched location
@@ -178,6 +182,14 @@ function initMap() {
         for (i = 0; i < markers.length; i++) {
             markers[i].setMap(null);
         }
+
+    };
+
+    function removeLocationMarkers() {
+        for (i = 0; i < markerLocation.length; i++) {
+            markerLocation[i].setMap(null);
+        }
+
     };
 };
 
